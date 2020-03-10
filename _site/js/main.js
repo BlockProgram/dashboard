183a4da6360999b9e05fe881a1b81d1e1470c9b7
BlockProgram <blockprogram@protonmail.com>
2020-03-10 22:22:35 +0100

    update transition sidemenu

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,72 +1,70 @@
 document.addEventListener("DOMContentLoaded", function() {
   // FORTUNE DASHBOARD : Take input from TextArea, print it to page, save it to localStorage,
   // calculate percentage and save to localStorage. Then synchronize % & progress bar
-    let inputAmount     = document.querySelector("#input-amount");
-    let percentage      = document.querySelector("#percentage-fortune");
-    let goalAmount      = document.getElementById("goal-amount").textContent;
-    let progressBar     = document.querySelector(".actualprogress-obj3"); 
-    let localInput      = localStorage.getItem("input"); 
-    let localPercentage = localStorage.getItem("percentage"); 
 
-    inputAmount.innerHTML   = localInput;
-    percentage.innerHTML    = localPercentage;
-    progressBar.style.width = localPercentage +'%'; 
+  let inputAmount = document.querySelector("#input-amount");
+  let percentage = document.querySelector("#percentage-fortune");
+  let goalAmount = document.getElementById("goal-amount").textContent;
+  let progressBar = document.querySelector(".actualprogress-obj3");
+  let localInput = localStorage.getItem("input");
+  let localPercentage = localStorage.getItem("percentage");
 
-   function calculateThenStore(event) { 
+  inputAmount.innerHTML = localInput;
+  percentage.innerHTML = localPercentage;
+  progressBar.style.width = localPercentage + "%";
 
-        let textArea = new Number(event.target.amount.value);
-        localStorage.setItem("input", textArea);
-        let roundPercentage = Math.round(textArea / goalAmount * 100); 
-        localStorage.setItem("percentage", roundPercentage); 
-  
-    }  
-    let button = document.querySelector("#input-fortune");
-    button.addEventListener("submit", calculateThenStore);
+  function calculateThenStore(event) {
+    let textArea = new Number(event.target.amount.value);
+    localStorage.setItem("input", textArea);
+    let roundPercentage = Math.round((textArea / goalAmount) * 100);
+    localStorage.setItem("percentage", roundPercentage);
+  }
+  let button = document.querySelector("#input-fortune");
+  button.addEventListener("submit", calculateThenStore);
 
-  //ADD COLOR SWITCHES 
-    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-    const currentTheme = localStorage.getItem('theme');
+  //ADD COLOR SWITCHES
 
-    if (currentTheme) {
-        document.documentElement.setAttribute('data-theme', currentTheme);
-    
-        if (currentTheme === 'dark') {
-            toggleSwitch.checked = true;
-        }
+  const toggleSwitch = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+  );
+  const currentTheme = localStorage.getItem("theme");
+
+  if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+      toggleSwitch.checked = true;
     }
+  }
 
-    function switchTheme(e) {
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        }
-        else {        document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        }    
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
     }
+  }
 
-    toggleSwitch.addEventListener('change', switchTheme, false);
-    
+  toggleSwitch.addEventListener("change", switchTheme, false);
 
-  //SAVE CHECKED CHECKBOXES 
+  //SAVE CHECKED CHECKBOXES
   // https://www.sitepoint.com/quick-tip-persist-checkbox-checked-state-after-page-reload/
-    
-    let checkboxes = document.querySelectorAll(".check-dash"); 
 
-    var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
+  var checkboxValues = JSON.parse(localStorage.getItem("checkboxValues")) || {},
     $checkboxes = $(".check-dash");
 
-    $checkboxes.on("change", function(){
-      $checkboxes.each(function(){
-        checkboxValues[this.id] = this.checked;
-      });
-      
-      localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+  $checkboxes.on("change", function() {
+    $checkboxes.each(function() {
+      checkboxValues[this.id] = this.checked;
     });
 
-    // On page load
-    $.each(checkboxValues, function(key, value) {
-      $("#" + key).prop('checked', value);
-    });
+    localStorage.setItem("checkboxValues", JSON.stringify(checkboxValues));
+  });
 
+  // On page load
+  $.each(checkboxValues, function(key, value) {
+    $("#" + key).prop("checked", value);
+  });
 });
